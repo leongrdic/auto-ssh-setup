2025f28c8e62123fb5605f8547f3f4463420bcd6
Leon <leongrdic@gmail.com>
2017-11-08 21:30:09 +0100

    first commit yay

---
+++ README.md
@@ -0,0 +1,50 @@
+# auto-host-setup
+
+Set up your new server in a matter of seconds.
+I found myself always doing the same procedure while setting up a new server, so I wrote this little script that automates it.
+
+Let's say you're on __host A__ and want to set up the __host B__. When you execute the script on __A__, this is what it basically does the remote server (__B__):
+-   creates a new user with your username (from __A__)
+-   adds the new user to sudo group
+-   installs your public SSH key (from __A__; can be specified)
+-   configures SSH with secure settings and restarts the service
+-   disables root password
+
+So, as you can see, it's simple but effective and saves time.
+
+## Requirements
+-   you must NOT run the script as `root` on the local host
+-   your account on the local host is required to have an SSH key, if you don't have one use `ssh-keygen`
+-   the remote server must have `sudo` installed
+-   the remote account you're connecting with has to be in the `sudo` group (or be `root`)
+
+
+## Installation
+```
+cd ~
+git clone https://github.com/leongrdic/auto-ssh-setup.git
+cd auto-host-setup
+chmod +x setup.sh
+```
+
+## Usage
+```
+./setup.sh [options] [user@]hostname
+
+# to view the full list of options use:
+./setup.sh -h
+```
+
+## SSH settings
+The script modifies the `/etc/ssh/sshd_config` config setting the following options
+```
+PermitRootLogin no
+PubkeyAuthentication yes
+PermitEmptyPasswords no
+PasswordAuthentication no
+```
+
+## Conclusion
+Since I've only been able to test this script under Ubuntu/Debian, any feedback would be helpful.
+
+Also, feel free to contribute by opening issues if you found any problems. PRs are welcome!
---
+++ setup.sh
@@ -0,0 +1,116 @@
+#/bin/bash
+
+function echoexit {
+  echo $1
+  echo
+  exit $2;
+}
+
+echo
+echo "auto-ssh-setup"
+
+if [ -z `which ssh` ]; then
+  echoexit "ssh not found. is it in your PATH?" 1
+fi
+
+username="$(whoami)"
+keyfile="$HOME/.ssh/id_rsa.pub"
+port=22
+
+if [ "$username" == "root" ]; then
+  echoexit "please run me as a regular user, not root" 1
+fi
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "./setup.sh [options] [user@]hostname"
+      echo " "
+      echo "options:"
+      echo "-h, --help                show this text"
+      echo "-k PATH                   specify the ssh public key path"
+      echo "-p PORT                   specify the ssh port number"
+      echo "-u USERNAME               use a username different than yours"
+      echo
+      exit 0
+      ;;
+    -k)
+      shift
+      if test $# -gt 0; then
+        export keyfile=$1
+      else
+        echoexit "no keyfile specified" 1
+      fi
+      shift
+      ;;
+    -p)
+      shift
+      if test $# -gt 0; then
+        export port=$1
+      else
+        echoexit "no port number specified" 1
+      fi
+      shift
+      ;;
+    -u)
+      shift
+      if test $# -gt 0; then
+        export username=$1
+      else
+        echoexit "no username specified" 1
+      fi
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+hostname=$1
+
+if [ -z "$hostname" ]; then
+  echoexit "missing hostname, if you need help: ./setup -h" 1
+fi
+
+key="$(cat $keyfile 2>/dev/null)"
+
+if [ -z "$key" ]; then
+  echoexit "keyfile not found, or is empty" 1
+fi
+
+echo "connecting..."
+ssh -t -p$port $hostname "
+echo \"creating a remote user...\";
+sudo useradd -m -s /bin/bash $username;
+sudo passwd $username;
+
+echo \"configuring user...\";
+sudo usermod -a -G sudo $username;
+sudo -u$username mkdir -p /home/$username/.ssh;
+sudo -u$username touch /home/$username/.ssh/authorized_keys;
+sudo -u$username echo \"$key\" >> /home/$username/.ssh/authorized_keys;
+
+echo \"configuring ssh...\";
+sudo perl -pi -e 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config;
+sudo perl -pi -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config;
+sudo perl -pi -e 's/PermitRootLogin without-password/PermitRootLogin no/g' /etc/ssh/sshd_config;
+
+sudo perl -pi -e 's/#PubkeyAuthentication/PubkeyAuthentication/g' /etc/ssh/sshd_config;
+sudo perl -pi -e 's/PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/sshd_config;
+
+sudo perl -pi -e 's/#PermitEmptyPasswords/PermitEmptyPasswords/g' /etc/ssh/sshd_config;
+sudo perl -pi -e 's/PermitEmptyPasswords yes/PermitEmptyPasswords no/g' /etc/ssh/sshd_config;
+
+sudo perl -pi -e 's/#PasswordAuthentication/PasswordAuthentication/g' /etc/ssh/sshd_config;
+sudo perl -pi -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config;
+
+echo \"restarting ssh service...\";
+sudo service ssh restart;
+
+echo \"disabling root password...\";
+sudo passwd -l root > /dev/null;
+"
+
+echo
+echoexit "done. if there was no errors above, the server is ready!" 0
